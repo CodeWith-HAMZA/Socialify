0763bdbac0c597856a6c1d666531097d78073bc2
Muhammad Hamza Jawaid Shaikh <115408642+CodeWith-HAMZA@users.noreply.github.com>
2023-11-10 22:05:29 +0500

    fix - cleaning

--- src/app/(root)/reels/page.tsx
+++ src/app/(root)/reels/page.tsx
@@ -1,17 +1,26 @@
 import ReelVideoCard from "@/components/cards/ReelVideoCard";
 import ReelVideosContainer from "@/components/containers/ReelVideosContainer";
 
+function Reels() {
+  const reelUrl =
+    "https://utfs.io/f/a229d104-d4e0-45a7-b271-2040ef2f0d7a-3a.mp4";
+  return (
+    <ReelVideosContainer>
+      <ReelVideoCard videoSrc={reelUrl} />
+      <ReelVideoCard videoSrc={reelUrl} />
+      <ReelVideoCard videoSrc={reelUrl} />
+      <ReelVideoCard videoSrc={reelUrl} />
+      <ReelVideoCard videoSrc={reelUrl} />
+    </ReelVideosContainer>
+  );
+}
+
 const ReelsPage = () => {
   return (
     <section className="pt-3 pl-4">
       <h1 className="font-bold text-2xl">Reels</h1>
-
-      <ReelVideosContainer>
-        <ReelVideoCard videoSrc="/v.mp4" />
-        <ReelVideoCard videoSrc="/v.mp4" />
-        <ReelVideoCard videoSrc="/v.mp4" />
-        <ReelVideoCard videoSrc="/v.mp4" />
-      </ReelVideosContainer>
+      {/* Reels  */}
+      <Reels></Reels>
     </section>
   );
 };
